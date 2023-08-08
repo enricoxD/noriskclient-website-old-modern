31490f510e2af4d2aa6c6f9b4d91cb657c7333da
enricoxD <noreply@enricoe.de>
2023-08-08 16:47:37 +0200

    render thumbnail and only load iframe after clicking

--- src/components/global/YouTubePlayer.tsx
+++ src/components/global/YouTubePlayer.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+"use client"
+import React, {useState} from "react";
+import Image from "next/image";
 
-export const YouTubePlayer = ({id}: { id: string }) => (
-  <div className="youtube-player">
-    <iframe
-      loading={"lazy"}
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${id}`}
-      frameBorder="0"
-      // allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
-);
+export const YouTubePlayer = ({id}: { id: string }) => {
+  const [showVideo, setShowVideo] = useState(false);
+
+  const playVideo = () => {
+    setShowVideo(true);
+  };
+
+  return (
+    <div className="youtube-player">
+      {
+        showVideo ?
+          <iframe
+            loading={"lazy"}
+            src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+            allowFullScreen
+            title="Embedded youtube"
+          />
+          :
+          <div className="youtube-thumbnail" onClick={playVideo}>
+            <Image src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`} alt={"YouTube Video"} fill/>
+            <div className="play-button-overlay"></div>
+          </div>
+      }
+    </div>
+  );
+}
