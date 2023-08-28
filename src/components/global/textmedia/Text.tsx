9a83701575ea9dc3a3119c66037502ba21b0269f
enricoxD <noreply@enricoe.de>
2023-08-28 18:21:18 +0200

    add an option to make text align left

--- src/components/global/textmedia/Text.tsx
+++ src/components/global/textmedia/Text.tsx
@@ -7,13 +7,14 @@ export interface TextProps {
   buttonHref?: string;
   buttonLabel?: string;
   buttonIcon?: string;
+  leftText?: boolean;
 }
 
-export const Text = ({title, titleClassName, text, buttonHref, buttonLabel, buttonIcon}: TextProps) => {
+export const Text = ({title, titleClassName, text, leftText, buttonHref, buttonLabel, buttonIcon}: TextProps) => {
   return (
     <div className={"text"}>
       {title && <p className={`title ${titleClassName}`}>{title}</p>}
-      <p className={"text-content"} dangerouslySetInnerHTML={{__html: text}} />
+      <p className={`text-content ${leftText ? "left-text" : ""}`} dangerouslySetInnerHTML={{__html: text}} />
       { buttonHref && buttonLabel &&
           <Button href={buttonHref} icon={buttonIcon}>
             {buttonLabel}
